3f8e237dd42c5355119117d72fcb2a86275d8969
A-V-Kuzmich <a.v.kuzmi4@gmail.com>
2022-02-05 20:18:31 +0200

    add style and form Login and Registration

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,3 +1,64 @@
+import { useState } from 'react';
+import { Notification } from '../../components/Notification';
+import { useCreateContactMutation, useGetContactsQuery } from 'redux/contact/contact-operation';
+
+import s from './Login.module.scss';
+
 export default function Login() {
-  return <h2>Login</h2>;
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [addContact] = useCreateContactMutation();
+  const { data } = useGetContactsQuery();
+
+  const handleSubmitt = e => {
+    e.preventDefault();
+
+    if (checkName(name)) {
+      return Notification(name);
+    }
+    addContact({ name, phone });
+    setName('');
+    setPhone('');
+  };
+
+  const checkName = newName => {
+    return data.find(({ name }) => name.toLowerCase() === newName.toLowerCase());
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmitt} className={s.form}>
+        <label>
+          <input
+            className={s.name}
+            type="text"
+            name="name"
+            value={name}
+            onChange={({ currentTarget: { value } }) => setName(value)}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            placeholder="Mail"
+          />
+        </label>
+        <label>
+          <input
+            className={s.number}
+            type="tel"
+            name="number"
+            value={phone}
+            onChange={({ currentTarget: { value } }) => setPhone(value)}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            placeholder="Password"
+          />
+        </label>
+
+        <button type="submit" className={s.button}>
+          Sign in
+        </button>
+      </form>
+    </>
+  );
 }
